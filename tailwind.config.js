a20165a886412eb8e6ec870722209d8f389a8a55
erfan578 <er000578@gmail.com>
2023-04-19 16:40:42 +0430

    add project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        vazirmatn: 'Vazirmatn',
+        poppins: 'Poppins'
+      }
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+}
